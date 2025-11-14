251684afc7238b616188864922488baeda747a07
Felipe Santos Ferreira <minuguaxi@gmail.com>
2025-11-14 18:45:53 -0300

    a

--- db/db.go
+++ db/db.go
@@ -2,11 +2,12 @@ package db
 
 import (
 	"database/sql"
+
 	_ "github.com/lib/pq"
 )
 
 func Acesso() *sql.DB {
-	var conexao = "user=postgres dbname=meu_banco host=localhost password=123 sslmode=disable"
+	var conexao = "user=dba dbname=meubanco host=localhost password=123 sslmode=disable"
 	db, err := sql.Open("postgres", conexao)
 	if err != nil {
 		panic(err)
